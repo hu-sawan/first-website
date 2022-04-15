04ca33c2b9ac120a2862cda388d20e7c0b5366b5
Hussein Sawan <96922361+hu-sawan@users.noreply.github.com>
2022-04-15 15:49:39 +0300

    Update upgrade.js

--- js/upgrade.js
+++ js/upgrade.js
@@ -2,7 +2,7 @@ let section = document.querySelector(".skills");
 let progress = document.querySelectorAll(".prog span");
 
 window.onscroll = function () {
-	if (window.scrollY >= section.offsetTop - 250) {
+	if (window.scrollY >= section.offsetTop - 300) {
 		progress.forEach((span) => {
 		span.style.width = span.dataset.width;
 		});
